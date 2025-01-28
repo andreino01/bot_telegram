0181111a66f95a6667b1c3f9d436d99bc8357ec5
andreino01 <mallianiandrea@gmail.com>
2025-01-29 00:58:05 +0100

    Update bot_LP.py

--- bot_LP.py
+++ bot_LP.py
@@ -30,8 +30,8 @@ gc = gspread.authorize(creds)
 sh = gc.open_by_key(os.environ.get('SHEET_ID'))
 
 # Lista degli utenti registrati
-saved_chat_ids = [637735039]
-saved_chat_ids2 = [1832764914, 5201631829, 637735039, 700212414]
+saved_chat_ids2 = [637735039]
+saved_chat_ids = [1832764914, 5201631829, 637735039, 700212414]
 
 # Domande del quiz
 DOMANDE = [
@@ -108,7 +108,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("🎉 Quiz completato! Risposte salvate.")
 
 	 # Determina l'URL del grafico per l'utente
-        if chat_id == 1832764914 or chat_id == 637735039:
+        if chat_id == 1832764914:
             chart_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTZnK4kFwfA4EONo5mKHz32uk2QS0OHzgW6suVPz2EwgHnaWilA9z07NRJ_gmjZD83ri89NpaZtDIIv/pubchart?oid=1293144718&format=image"
         elif chat_id == 5201631829:
             chart_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTZnK4kFwfA4EONo5mKHz32uk2QS0OHzgW6suVPz2EwgHnaWilA9z07NRJ_gmjZD83ri89NpaZtDIIv/pubchart?oid=36108840&format=image"
@@ -168,9 +168,8 @@ def get_soldi_spesi(chat_id):
     # Mappa degli ID e i fogli corrispondenti
     sheet_map = {
         1832764914: 1,  # Foglio 2
-        637735039: 3,
         5201631829: 2,  # Foglio 3
-        #700212414: 3    # Foglio 4
+        700212414: 3    # Foglio 4
     }
 
     # Verifica se l'ID dell'utente è nella mappa

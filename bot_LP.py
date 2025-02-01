fcfc6be0e21c8012f771dff6f1f03ff874fdf575
andreino01 <mallianiandrea@gmail.com>
2025-02-02 00:56:34 +0100

    Update bot_LP.py

--- bot_LP.py
+++ bot_LP.py
@@ -248,7 +248,7 @@ def get_improvement_status(chat_id):
     
     # Leggi la cella W5 (modifica questo riferimento se cambia posizione nello sheet)
     #status_cell = int(worksheet.cell(6, 24).value) # X6 = riga 6, colonna 24
-	status_cell = worksheet.cell(6, 24).value
+    status_cell = worksheet.cell(6, 24).value
     return status_cell
 
 def today_zero(chat_id):

dd29d21d87f7b94a2e9767e4e99e336d8473ad48
andreino01 <mallianiandrea@gmail.com>
2025-02-16 14:34:16 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -582,7 +582,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 # Endpoint per ricevere gli update dal webhook
-@app.post("/{TOKEN}")
+@app.post("/{token}")
 async def process_update(request: Request, token: str):
     if token != TOKEN:
         return Response(status_code=HTTPStatus.FORBIDDEN)

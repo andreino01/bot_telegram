6747af248f7cb6da717fd1f4e08ad3549dc86ac9
andreino01 <mallianiandrea@gmail.com>
2025-02-17 13:37:11 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,13 +38,13 @@ SPREADSHEET_ID = os.environ.get('SHEET_ID')
 sh = gc.open_by_key(SPREADSHEET_ID)
 
 # Lista degli utenti registrati
-saved_chat_ids = [637735039]
-saved_chat_ids2 = [1832764914, 5201631829, 700212414]
+saved_chat_ids2 = [637735039]
+saved_chat_ids = [1832764914, 5201631829, 700212414]
 
 # Mappa degli ID e i fogli corrispondenti
 sheet_map = {
-    637735039: 1,
-    #1832764914: 1,  # Foglio 2
+    #637735039: 1,
+    1832764914: 1,  # Foglio 2
     5201631829: 2,  # Foglio 3
     700212414: 3    # Foglio 4
 }
@@ -390,7 +390,7 @@ def create_keyboard(buttons):
     
 def get_grafico_url(chat_id, tipo):
     grafici = {
-        637735039: {#1832764914
+        1832764914: {
             "giornaliero": "https://docs.google.com/spreadsheets/d/e/2PACX-1vTZnK4kFwfA4EONo5mKHz32uk2QS0OHzgW6suVPz2EwgHnaWilA9z07NRJ_gmjZD83ri89NpaZtDIIv/pubchart?oid=1293144718&format=image",
             "settimanale": "https://docs.google.com/spreadsheets/d/e/2PACX-1vTZnK4kFwfA4EONo5mKHz32uk2QS0OHzgW6suVPz2EwgHnaWilA9z07NRJ_gmjZD83ri89NpaZtDIIv/pubchart?oid=1154554874&format=image"
         },
@@ -698,4 +698,4 @@ application.add_handler(CallbackQueryHandler(handle_button_click))
 if __name__ == "__main__":
     print("Bot avviato in modalità polling...")
     setup_job_queue(application)
-    application.run_polling(poll_interval=0.2)
+    application.run_polling()
